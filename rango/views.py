f9a97dffdbb4ab11545321c24fa5533726ae0330
nindavidw <davidwang@techie.com>
2017-05-22 10:38:57 +0800

    add about page

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!" + urls)
+	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+	return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+	context_dict = {'name': 'David'}
+
+	return render(request, 'rango/about.html', context=context_dict)
